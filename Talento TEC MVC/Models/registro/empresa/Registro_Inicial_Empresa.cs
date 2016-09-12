41b8c46acf98dcd28bfa574c8728e90830f2b743
OswaldoD <dreammicro7@hotmail.com>
2016-09-12 01:20:30 -0600

    d

--- Talento TEC MVC/Models/registro/empresa/Registro_Inicial_Empresa.cs
+++ Talento TEC MVC/Models/registro/empresa/Registro_Inicial_Empresa.cs
@@ -8,12 +8,12 @@ namespace Talento_TEC_MVC.Models.registro.empresa
 {
     public class Registro_Inicial_Empresa
     {
-        /*
+        
         [Required]
         [Display(Name = "Nombre comercial de la empresa  (*)")]
         [DataType(DataType.Text)]
         public string nombreEmpresa { get; set; }
-
+        /*
         [Required]
         [Display(Name = "Nombre comercial de la empresa  (*)")]
         [DataType(DataType.Text)]
