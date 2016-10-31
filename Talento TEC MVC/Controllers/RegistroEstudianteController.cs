a9ffcc29849a91e53a2535c24ed241ca1359bb97
OswaldoD <dreammicro7@hotmail.com>
2016-10-30 21:57:16 -0600

    estudiantes

--- Talento TEC MVC/Controllers/RegistroEstudianteController.cs
+++ Talento TEC MVC/Controllers/RegistroEstudianteController.cs
@@ -24,7 +24,9 @@ namespace Talento_TEC_MVC.Controllers
         [AllowAnonymous]
         public ActionResult datos_personales(datos_personales model)
         {
-            return View();
+            //return View();
+            return RedirectToAction("informacion_contacto", "registroestudiante");
+
         }
 
         public ActionResult informacion_contacto()
@@ -32,24 +34,80 @@ namespace Talento_TEC_MVC.Controllers
             return View();
         }
 
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult informacion_contacto(informacion_contacto model)
+        {
+            return RedirectToAction("educacion_formal", "registroestudiante");
+
+        }
+
         public ActionResult educacion_formal()
         {
             return View();
         }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult educacion_formal(educacion_formal model)
+        {
+            return RedirectToAction("dominio_idioma", "registroestudiante");
+        }
+
 
         public ActionResult dominio_idioma()
         {
             return View();
         }
 
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult dominio_idioma(dominio_idioma model)
+        {
+            return RedirectToAction("experiencia_laboral", "registroestudiante");
+        }
+
+
         public ActionResult experiencia_laboral()
         {
             return View();
         }
 
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult experiencia_laboral(experiencia_laboral model)
+        {
+            return RedirectToAction("conocimientos_referencias", "registroestudiante");
+
+        }
+
+
         public ActionResult conocimientos_referencias()
         {
             return View();
         }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult conocimientos_referencias(conocimientos_referencias model)
+        {
+            return RedirectToAction("finalizar", "registroestudiante");
+
+        }
+
+        public ActionResult finalizar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult finalizar(string url)
+        {
+            return RedirectToAction("Index", "Home");
+
+        }
+
+
     }
 }
